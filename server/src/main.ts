382b99d77002842a1731677a0735c85fadbfa60a
wasif <wasif0332@gmail.com>
2020-11-04 18:57:06 +0500

    setting up server

---
+++ server/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {ValidationPipe} from "@nestjs/common";
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  const options = new DocumentBuilder()
+      .setTitle('Resume')
+      .setDescription('The Resume API description')
+      .setVersion('1.0')
+      .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(app.get('ConfigService').get('port'));
+}
+bootstrap();
